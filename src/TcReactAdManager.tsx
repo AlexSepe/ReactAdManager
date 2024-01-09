7787e5272cdc0542c37a96ddc50a035d1af5a97f
AlexSepe <alexandre.sepe2@gmail.com>
2024-01-09 16:53:45 -0300

    Impl. Fluxo workaround para destruir slots divIds duplicados. Adicionado divId

--- src/TcReactAdManager.tsx
+++ src/TcReactAdManager.tsx
@@ -8,6 +8,28 @@ export class TcReactAdManager extends Component<TcReactAdManagerContainerProps> 
     static counter = 0;
     private unitSlotName = "";
     private readonly eventImpressionViewableHandler = this.eventImpressionViewable.bind(this);
+
+    private destroySlotsSameId(slotElementId: string): void {
+        // Workaround para destruir slots com nomes duplicados - ocorre em caso de refresh Mendix, onde o react-ad-manager não recebe o dismount e não destroy por lá
+        // Então antes de criar um novo AdSlot acessamos direto o DOM do GPT e destruimos!!!
+        // window.googletag.pubads().getSlots().find((slot) => slot.getSlotElementId() === "div-ad-02")
+        const googletag = window.googletag;
+        if (googletag) {
+            try {
+                const slot = window.googletag
+                    .pubads()
+                    .getSlots()
+                    .find((slot: { getSlotElementId: () => string }) => slot.getSlotElementId() === slotElementId);
+                if (slot) {
+                    console.log("⛔ destroying slot " + slotElementId);
+                    googletag.destroySlots([slot]);
+                }
+            } catch (error) {
+                console.warn(error);
+            }
+        }
+    }
+
     render(): ReactNode {
         // Check https://socket.dev/npm/package/react-ad-manager
 
@@ -21,6 +43,7 @@ export class TcReactAdManager extends Component<TcReactAdManagerContainerProps> 
             this.unitSlotName =
                 this.props.slotName?.value || "div-add-" + this.props.name + "_" + TcReactAdManager.counter++;
         }
+        this.destroySlotsSameId(this.unitSlotName);
         return (
             <Ad
                 adUnit={this.props.adUnit.value || ""}
